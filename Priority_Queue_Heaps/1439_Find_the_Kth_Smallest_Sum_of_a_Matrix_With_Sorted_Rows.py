b0d41029c62c94629aa794eee383292ca11e2e33
Kartek Jadhav <kartekjadhav2810@gmail.com>
2024-02-28 02:40:24 +0530

    Added 1439_Find_the_Kth_Smallest_Sum_of_a_Matrix_With_Sorted_Rows.py

---
+++ Priority_Queue_Heaps/1439_Find_the_Kth_Smallest_Sum_of_a_Matrix_With_Sorted_Rows.py
@@ -0,0 +1,21 @@
+#TC - K*log(m * n^m) 
+class Solution:
+    def kthSmallest(self, mat: List[List[int]], k: int) -> int:
+        m,n = len(mat), len(mat[0])
+        temp = [0]*m
+        total = sum([mat[i][0] for i in range(m)])
+        hp = [(total,temp)]
+        ans = 0
+        visited = set()
+        while k>0 and hp:
+            ans, col = heapq.heappop(hp)
+            for i in range(m):
+                res = col[:]
+                if res[i]+1<n: 
+                    res[i]+=1
+                    new_sum = ans - mat[i][col[i]] + mat[i][res[i]]
+                    if tuple(res) not in visited:
+                        heapq.heappush(hp,(new_sum,res))
+                        visited.add(tuple(res))
+            k-=1
+        return ans
