e7c3a9ee57cd4e610d407e8134df0a7853646314
Kartek Jadhav <kartekjadhav2810@gmail.com>
2024-03-22 00:20:16 +0530

    Modified 206_Reverse_Linked_List.py

--- Linked_List/206_Reverse_Linked_List.py
+++ Linked_List/206_Reverse_Linked_List.py
@@ -22,11 +22,6 @@ class Solution:
 #TC - O(N)
 #SC - O(1)
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head: return head
