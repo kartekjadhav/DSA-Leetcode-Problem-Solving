5aec78405b3c46db2fa74af84b6243ff9fdf219f
Kartek Jadhav <kartekjadhav2810@gmail.com>
2024-03-27 10:25:10 +0530

    Created Recursion folder and added 78_Subsets.py

---
+++ Recursion/78_Subsets.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def helper(i):
+            if i==len(nums):
+                ans.append(ds[:])
+                return
+            ds.append(nums[i])
+            helper(i+1)
+            ds.pop()
+            helper(i+1)
+
+        ds = []
+        ans = []
+        helper(0)
+        return ans
