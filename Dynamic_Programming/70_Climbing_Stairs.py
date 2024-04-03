7ae2a078240b98f791ce626c738bb241c66a18d6
Kartek Jadhav <kartekjadhav2810@gmail.com>
2024-04-03 11:27:26 +0530

    Added Bottom up approach for 70_Climbing_Stairs.py

--- Dynamic_Programming/70_Climbing_Stairs.py
+++ Dynamic_Programming/70_Climbing_Stairs.py
@@ -1,3 +1,5 @@
+#1. Top Down approach (Recursion) 
+
 #TC - O(N)
 #SC - Extra - O(N), Stack - O(N)
 
@@ -12,4 +14,27 @@ class Solution:
         dp = [-1]*(n+1)
         dp[0] = 1
         return helper(n)
-        
+
+#2. Bottom Up Approach (Iterative)
+
+#TC - O(N)
+#SC - O(1)
+
+#USING ARRAY FOR STORAGE (SC - O(N))
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [-1]*(n+1)
+        dp[0],dp[1] = 1,1
+        for i in range(2,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[-1]
+    
+#Without Using Array (SC - O(1))
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a,b = 1,1
+        for i in range(2,n+1):
+            temp = b
+            b = a + b
+            a = temp
+        return b
