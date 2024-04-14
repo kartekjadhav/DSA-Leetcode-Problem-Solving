532512e2d28ca24eb5d9fddaf3214ec0d2eb5e0b
Kartek Jadhav <kartekjadhav2810@gmail.com>
2024-04-14 12:11:25 +0530

    Added 404_Sum_of_Left_Leaves.py

---
+++ Binary_Trees/404_Sum_of_Left_Leaves.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from typing import Optional
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        def inorder(node,isLeft):
+            if not node: return 0
+            temp = 0
+            if isLeft and not node.left and not node.right:
+                temp = node.val
+            l = inorder(node.left,True)
+            r = inorder(node.right,False)
+            return l+r+temp
+
+        return inorder(root,False)
